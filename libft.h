3d05674ad071584ec0c19de69de6c47399d557d2
Nakedi MAFA <nmafa@c4r2s3.wethinkcode.co.za>
2019-05-30 10:07:08 +0200

    updated lib

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: nmafa <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 14:22:29 by nmafa             #+#    #+#             */
-/*   Updated: 2019/05/28 13:43:51 by nmafa            ###   ########.fr       */
+/*   Updated: 2019/05/29 12:25:27 by nmafa            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,16 @@ void	ft_putstr(char const *str);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char const *str, int fd);
 
-int	ft_toupper(int c);
-int	ft_tolower(int c);
-int	ft_isascii(int c);
-int	ft_isdigit(int c);
-int ft_isalpha(int c);
-int ft_isalnum(int c);
-int ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isalpha(int c);
+int		ft_isalnum(int c);
+int		ft_isprint(int c);
+int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 char	*ft_strcpy(char *dst, const char *src);
+char	*ft_strncpy(char *dst, const char *src, size_t n);
 #endif
