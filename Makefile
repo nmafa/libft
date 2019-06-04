c8ca2f5a3102a970126928626c1bf87a199b1dd2
Nakedi MAFA <nmafa@c4r7s3.wethinkcode.co.za>
2019-06-04 17:22:35 +0200

    updated

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: nmafa <marvin@42.fr>                       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/06/03 16:27:06 by nmafa             #+#    #+#              #
-#    Updated: 2019/06/03 17:16:04 by nmafa            ###   ########.fr        #
+#    Updated: 2019/06/04 17:13:44 by nmafa            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,6 +18,8 @@ SRC = ft_putchar.c\
 	  ft_putstr.c\
 	  ft_putchar_fd.c\
 	  ft_putstr_fd.c\
+	  ft_putendl.c\
+	  ft_putendl_fd.c\
 	  ft_toupper.c\
 	  ft_toupper.c\
 	  ft_ascii.c\
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: nmafa <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 14:22:29 by nmafa             #+#    #+#             */
-/*   Updated: 2019/05/29 12:25:27 by nmafa            ###   ########.fr       */
+/*   Updated: 2019/06/04 17:13:09 by nmafa            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ void	ft_putchar(char c);
 void	ft_putstr(char const *str);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char const *str, int fd);
+void	ft_putendl(char const *s);
+void	ft_putendl_fd(char const *s, int fd);
 
 int		ft_toupper(int c);
 int		ft_tolower(int c);
