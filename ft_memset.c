7dcacface976c70c29df504c4826981dfae78e79
Nakedi MAFA <nmafa@c4r8s2.wethinkcode.co.za>
2019-06-06 16:41:19 +0200

    added

--- ft_memset.c
+++ ft_memset.c
@@ -6,8 +6,23 @@
 /*   By: nmafa <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 18:51:13 by nmafa             #+#    #+#             */
-/*   Updated: 2019/06/05 18:51:36 by nmafa            ###   ########.fr       */
+/*   Updated: 2019/06/06 16:33:00 by nmafa            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char	*n;
+	int				i;
+
+	i = 0;
+	n = (unsigned char *)b;
+	while (i < (int)len)
+	{
+		n[i] = c;
+		i++;
+	}
+	return (b);
+}
