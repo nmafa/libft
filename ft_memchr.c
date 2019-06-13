f72349a077ff0b96978d58ce0c2aaa149668e28b
Nakedi MAFA <nmafa@c4r1s5.wethinkcode.co.za>
2019-06-13 18:24:08 +0200

    added

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,11 +6,31 @@
 /*   By: nmafa <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 11:30:17 by nmafa             #+#    #+#             */
-/*   Updated: 2019/06/13 15:45:24 by nmafa            ###   ########.fr       */
+/*   Updated: 2019/06/13 18:20:06 by nmafa            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		memcmp(const void *s1, const void *s2, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
+	unsigned char *ret;
+
+	ret = (unsigned char *)s;
+	while(n){
+		if(*ret == (unsigned char)c){
+			return (ret);
+		}else{
+			ret++;
+		}
+		n--;
+	}
+	return (NULL);
+}
+
+//#include <stdio.h>
+
+//int main(){
+//	printf("%s", ft_memchr("this is where here", 'i', 15));
+//	return 0;
+//}
